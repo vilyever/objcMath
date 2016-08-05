7ec2ee69569737dde9df0f8cd4926bd512bccc4c
vilyever <250450035@163.com>
2016-08-05 15:57:10 +0800

    Initial Commit

---
+++ objcMath/objcMath.h
@@ -0,0 +1,19 @@
+//
+//  objcMath.h
+//  objcMath
+//
+//  Created by FTET on 16/8/5.
+//  Copyright © 2016年 vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for objcMath.
+FOUNDATION_EXPORT double objcMathVersionNumber;
+
+//! Project version string for objcMath.
+FOUNDATION_EXPORT const unsigned char objcMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objcMath/PublicHeader.h>
+
+
